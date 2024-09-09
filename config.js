f74433d47101074b4319c562d985f4c605e35dc1
Roudy-king <kingwhatsapp259@gmail.com>
2024-09-09 14:51:00 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG ||"https://telegra.ph/file/cad7038fe82e47f79c609.jpg",
+ALIVE_MSG: process.env.ALIVE_IMG ||"DILISHA-MD-NEW AVAIBLE NOW ⭕❤",
+};
