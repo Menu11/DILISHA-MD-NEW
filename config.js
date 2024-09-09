0922f7668f6b1863176626bf0faf9f1c087e5468
Roudy-king <kingwhatsapp259@gmail.com>
2024-09-09 18:38:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG ||"https://telegra.ph/file/cad7038fe82e47f79c609.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG ||"⭕NAME - DILISHA-MD-NEW 🇱🇰
-⭕SERVER - javascript 🐵
-⭕DEVELOPER - DILISHA ❤
-⭕DETAILE - ALLOW ACCESS
-⭕SRILANKAN NO1 WHATSAPP BOT",
+ALIVE_MSG: process.env.ALIVE_IMG ||"⭕DILISHA-MD-NEW⭕ RUN ⚡",
 };
