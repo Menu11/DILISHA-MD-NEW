29d0db8a1706bd9e2484bc287df54899ad79ce7a
Roudy-king <kingwhatsapp259@gmail.com>
2024-09-09 18:08:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
 connectToWA()
 }
 } else if (connection === 'open') {
-console.log('😼 Installing... ')
+console.log('🌟 Installing... ')
 const path = require('path');
 fs.readdirSync("./plugins/").forEach((plugin) => {
 if (path.extname(plugin).toLowerCase() == ".js") {
