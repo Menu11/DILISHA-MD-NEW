66bac3c5e1829297cd88588261aff36110272661
Roudy-king <kingwhatsapp259@gmail.com>
2024-09-09 22:13:46 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,12 +12,26 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `⭕*Uptime:*  ${runtime(process.uptime())}
-⭕*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-⭕*HostName:* ${os.hostname()}
-⭕*Owner:* ⚡Dilisha gimshan⚡
-⭕*Contact:* ⚡0775713391⚡
-⭕*TEAM OF:* ⚡FAMILY BOTS⚡
+let status = `⭕Uptime  ${runtime(process.uptime())}
+⭕Ram usage👉 ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+⭕HostName👉 ${os.hostname()}
+⭕Owner👉 ⚡Dilisha gimshan⚡
+⭕Contact👉 ⚡0775713391⚡
+⭕TEAM OF👉 ⚡FAMILY BOTS⚡
+⭕COMMAND 👇
+BOT CHECK - .alive
+BOT SETTING - .setting
+BOT DETAIL - .system
+BOT DOWNLOAD SONG - .song
+BOT DOWNLOAD VIDEO - .video
+BOT FACEBOOK DL - .fb "url"
+BOT APPLY SIZE - .apply "size"
+BOT OWNER DETAIL - .owner
+BOT FILM - .movie
+BOT MEDIAFIRE DOWNLOAD - .mediafire
+BOT GOOGLE DRIVE - .gd "link"
+👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆
+🤙🤙🤙🤙WOURLD NO 1 WHATSAPP BOT🤌🤌🤌
 `
 
 return reply(`${status}`)
