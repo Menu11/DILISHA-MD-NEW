1037c70b1083744192af557cfb426a9c3ddf0f48
Roudy-king <kingwhatsapp259@gmail.com>
2024-09-10 06:47:15 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -18,7 +18,49 @@ convert: '',
 search: ''
 };
 
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
+ }
+}
 
+let madeMenu = ` 👋⭕HELLO ${pushname}⭕👋
+> ⭕DOWNLOAD COMMANDS⭕ 📥
+
+${menu.download}
+
+> ⭕MAIN COMMANDS⭕ ⚡
+
+${menu.main}
+
+>⭕GROUP COMMANDS⭕ ⏬
+
+${menu.groups}
+
+>⭕OWNER COMMANDS⭕ 😈
+
+${menu.owner}
+
+> ⭕CONVERT COMMAND⭕ 🔄
+
+${menu.convert}
+
+>⭕SEARCH COMMAND⭕ 🥸
+
+${menu.search}
+
+>⭕MOVIE COMMAND⭕ 🎬
+
+${menu.movie}
+
+>⭕X VIDEO COMMAND⭕ 🥵
+
+${menu.xvideo}
+
+POWERD BY 'DILISHA GIMSHAN ⚡'
+`
+await conn.sendMessage(from,{image:{url:"https://telegra.ph/file/cad7038fe82e47f79c609.jpg"},caption:madeMenu},{quoted:mek})
+    
 }catch(e){
 console.log(e)
 reply(`${e}`)
