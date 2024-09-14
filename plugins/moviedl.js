7f759792ffcba4c126352a8b1a729f39cd1161fa
Roudy-king <kingwhatsapp259@gmail.com>
2024-09-14 17:10:32 +0530

    Create moviedl.js

---
+++ plugins/moviedl.js
@@ -0,0 +1,128 @@
+## SinhalaSub.lk.
+
+
+
+- Author : [Nimesh Official](https://github.com/nimeshofficiallk)
+
+
+
+***
+
+
+
+## Example
+
+```js
+
+const { getMoviesSearch, getMovieDL, getMovies } = require('sinhalasub.lk')
+
+
+
+(async () => {
+
+    const movies = await getMoviesSearch('Thalainagaram')
+
+    console.log(movies)
+
+})();
+
+```
+
+```js
+
+import { getMoviesSearch, getMovieDL, getMovies } from 'sinhalasub.lk'
+
+
+
+(async () => {
+
+    const movies = await getMoviesSearch('Thalainagaram')
+
+    console.log(movies)
+
+})();
+
+```
+
+***
+
+## OutPut
+
+```js
+
+{
+
+  "status": true,
+
+  "creator": "Nimesh Official",
+
+  "result": {
+
+    "title": "Thalainagaram 2 (2023) Sinhala Subtitles | සිංහල උපසිරසි සමඟ",
+
+    "imdb": "2.5",
+
+    "date": "Jun. 23, 2023",
+
+    "category": [
+
+      "Action",
+
+      "Crime",
+
+      "Drama"
+
+    ],
+
+    "description": "සුභ දවසක් හැමෝටම!!මේ අරං ආවේ Action/Crime/Drama ගනයට අයත් Thalainagaram 2 කියන සුපිරි ටැමිල් චිත්‍රපටයයි…එහෙනම් වැඩි කතා නැතුව මෙ සුපිරි චිත්‍රපට
+
+ිය රසවිඳින්න කියලා ඔයාලා හැමෝටම ආරාධනා කරනවා…ජය වේවා!!උපසිරසි නිර්මාණය  :- Sinhalasub.lk හි  හෂිනි ඉමාෂා සහෝදරිය…උපසිරසි හදල දෙන වෙබ්සයිට් එකෙන් හැමවෙලේම උපසිරසි ඩවු
+
+න්ලෝඩ් කරගන්න බලන්න ඒ වගේම උපසිරසි හදන අයව කමෙන්ට් කරල දිරිමත් කරන්න. ඒක මේ වෙබ්සයිට් එකේ විතරක් නෙමෙයි ඕනෙම එකක කරන්න . මොකද එයාල කාලය මහන්සිය කැපකරල ලොකු සේවයක් කර
+
+න්නෙ",
+
+    "image": "https://sinhalasub.lk/wp-content/uploads/2023/09/mNherq6NoBpGb5xySaAKtE3P3DN-200x300.jpg",
+
+    "dl_links": [
+
+      {
+
+        "quality": "FHD 1080p",
+
+        "size": "3.52 GB",
+
+        "link": "https://cdn.sldrive.fr/Movies/Tamil/Thalainagaram%202%20(2023)/Thalainagaram%202%20(2023)%20Tamil%20WEB-DL%201080p.mp4"
+
+      },
+
+      {
+
+        "quality": "HD 720p",
+
+        "size": "1.73 GB",
+
+        "link": "https://cdn.sldrive.fr/Movies/Tamil/Thalainagaram%202%20(2023)/Thalainagaram%202%20(2023)%20Tamil%20WEB-DL%20720p.mp4"
+
+      },
+
+      {
+
+        "quality": "SD 480p",
+
+        "size": "898 MB",
+
+        "link": "https://cdn.sldrive.fr/Movies/Tamil/Thalainagaram%202%20(2023)/Thalainagaram%202%20(2023)%20Tamil%20WEB-DL%20480p.mp4"
+
+      }
+
+    ]
+
+  }
+
+}
+
+```
+
+***
+
